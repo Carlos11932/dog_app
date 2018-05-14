4dc29d3a9e354d2fb6af797e77c2086b9cef8bde
carlos11932 <carlosbenito11293@gmail.com>
2018-05-14 20:24:57 +0200

    Add new views for dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -6,6 +6,50 @@ class DogsController < ApplicationController
 		@dogs = Dog.where.not(adopter_id: nil)
 	end
 
+	def show_dog
+		@dog = Dog.find(params['id'])
+		@publisher = User.find(@dog.publisher_id)
+	end
+
+	def show_my_dogs
+		@user = User.find(params['id'])
+		@dogs = Dog.where(publisher_id: params['id'])
+	end
+
+	def new_dog
+		
+	end
+
+	def create_dog
+		@dog = Dog.new(dog_params)
+    		if @dog.save
+      			flash[:success] = "Nuevo miembro en el equipo"
+      			redirect_to my_dogs_path(session[:current_user_name])
+    		else
+      			redirect_to my_dogs_path(session[:current_user_name])
+    		end
+	end
+
+	def edit_dog
+		@dog = Dog.find(params['id'])
+	end
+
+	def update_dog
+		@dog = Dog.find(params[:id])
+	    if @dog.update_attributes(dog_params)
+	      flash[:success] = "Perfil de #{@dog.name} actualizado"
+      		redirect_to my_dogs_path(session[:current_user_name])
+	    else
+      		redirect_to my_dogs_path(session[:current_user_name])
+	    end
+	end
+
+	def destroy_dog
+		@dog = Dog.find(params[:id])
+    	@dog.destroy
+    	redirect_to redirect_to my_dogs_path(session[:current_user_name]), :notice => "Perfil eliminado"
+	end
+
 	private
 
 		def check_log_in
@@ -13,4 +57,11 @@ class DogsController < ApplicationController
 				redirect_to 'home#index'
 			end
 		end
+
+		def dog_params
+    	  params.require(:dog).permit(:name, :birth_date, :breed,
+                                   :gender, :location_lat, :location_lng,
+                                   :description, :adopter_id, :publisher_id)
+    	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
 	post 'users/sign_in', to: 'users#sign_in'
 	# post 'users/sign_up', to: 'users#sign_up' 
 	get 'dogs', to: 'dogs#all_dogs', as: 'list_all_dogs'
+	get 'dogs/new', to: 'dogs#new_dog', as: 'new_dog'
+	get 'dogs/:id', to: 'dogs#show_dog', as: 'show_dog'
+	get 'dogs/published/:id', to: 'dogs#show_my_dogs', as: 'my_dogs'
+	get 'dogs/:id/edit', to: 'dogs#edit_dog', as: 'edit_dog'
+
+	post 'dogs/create_dog', to: 'dogs#create_dog'
+	post 'dogs/destroy_dog', to: 'dogs#destoy_dog'
 end
